4f8bb090607c209d800897cb78707e4d0b2c47ce
Atsushi Ishida <gipcompany@gmail.com>
2021-12-07 23:17:52 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # one-liners
+
+JavaScript Playground
+
+    curl -fsSL https://raw.githubusercontent.com/DiveIntoHacking/one-liners/blob/main/bin/javascript-playground.sh | bash
---
+++ bin/javascript-playground.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cat <<END_OF_INDEX_HTML > index.html
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JavaScript Playground</title>
+  </head>
+
+  <body>
+    <script src="javascript.js"></script>
+  </body>
+</html>
+END_OF_INDEX_HTML
+
+
+cat <<END_OF_JAVASCRIPT_JS > javascript.js
+console.log("Hello, JavaScript!")
+END_OF_JAVASCRIPT_JS
